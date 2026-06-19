cdb0f01fd99e8cb436edfa6e8c5bc587a14fba52
Dempsey845 <dempseynewton1@gmail.com>
2026-06-19 17:08:23 +0100

    Implemented raycast based shooting for the Player.

--- Player/States/Arm/arm_idle_state.gd
+++ Player/States/Arm/arm_idle_state.gd
@@ -1,6 +1,7 @@
 extends State
 
 @export var animation_manager: PlayerAnimationManager
+@export var shoot_ray: RayCast3D
 
 var can_shoot: bool = true
 
@@ -14,6 +15,22 @@ func update(_delta: float):
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		animation_manager.set_arm_state_machine_condition("shoot", true)
 		can_shoot = false
+		shoot()
+
+
+func shoot():
+	shoot_ray.force_raycast_update()
+
+	if shoot_ray.is_colliding():
+		var target: Node3D = shoot_ray.get_collider()
+		var hit_pos: Vector3 = shoot_ray.get_collision_point()
+
+		if target is Hurtbox:
+			var hurtbox: Hurtbox = target
+			hurtbox.register_hit(1)
+
+		print("Hit:", target.name)
+		print("Position:", hit_pos)
 
 func exit():
 	animation_manager.set_arm_state_machine_condition("pistol_idle", false)
