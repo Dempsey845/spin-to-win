2093dc48a3f012928cb276d59f227da461baf593
Dempsey845 <dempseynewton1@gmail.com>
2026-06-21 00:03:54 +0100

    Created a bullet projectile that the NPC fires.

--- NPC/Humanoid/npc_gun_manager.gd
+++ NPC/Humanoid/npc_gun_manager.gd
@@ -3,7 +3,17 @@ extends Node
 @export var revolver_visual: Node3D
 @export var hand_attachment_slot: Node3D
 
+@export var projectile_scene: PackedScene
+@export var fire_point: Marker3D
+
 func equip_revolver():
     revolver_visual.reparent(hand_attachment_slot)
     revolver_visual.position = Vector3.ZERO
     revolver_visual.rotation = Vector3.ZERO
+
+func shoot_projectile():
+    var projectile = projectile_scene.instantiate()
+    get_tree().current_scene.add_child(projectile)
+
+    projectile.global_position = fire_point.global_position
+    projectile.global_rotation = fire_point.global_rotation
---
+++ NPC/npc_bullet_projectile.gd
@@ -0,0 +1,13 @@
+extends Hitbox
+
+@export var projectile_speed: float = 10.0
+
+func _ready() -> void:
+    super._ready()
+
+func _process(delta: float) -> void:
+    global_position += -global_basis.z * projectile_speed * delta
+
+func _on_area_entered(area: Area3D):
+    super._on_area_entered(area)
+    queue_free()
