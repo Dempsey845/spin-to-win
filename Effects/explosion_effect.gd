402d38174f02720bfc0dd952a6c21a17796f4b23
Dempsey845 <dempseynewton1@gmail.com>
2026-06-21 17:59:55 +0100

    Created an expolsion effect.

---
+++ Effects/explosion_effect.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var debris: GPUParticles3D = $Debris
+@onready var smoke: GPUParticles3D = $Smoke
+@onready var fire: GPUParticles3D = $Fire
+
+func _ready() -> void:
+    await get_tree().create_timer(2.0).timeout
+    debris.emitting = true
+    smoke.emitting = true
+    fire.emitting = true
