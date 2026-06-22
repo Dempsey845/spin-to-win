93e7a7867de8b3af280b8f0611a03da35af61b18
Dempsey845 <dempseynewton1@gmail.com>
2026-06-22 17:24:03 +0100

    Added a wave timer label.

--- Player/player_revolver_manager.gd
+++ Player/player_revolver_manager.gd
@@ -9,40 +9,40 @@ signal revolver_picked_up
 var has_revolver: bool = true
 
 func _ready() -> void:
-    WaveSystem.instance.wave_ended.connect(func():
-        if !has_revolver:
-            revolver_status_animation_player.play("fade_in")
-    )
+	WaveSystem.instance.wave_ended.connect(func():
+		if !has_revolver:
+			revolver_status_animation_player.play("fade_in")
+	)
 
 func is_revolver_in_pickup() -> bool:
-    if has_revolver:
-        return false
-    
-    pickup_ray.force_raycast_update()
+	if has_revolver:
+		return false
+	
+	pickup_ray.force_raycast_update()
 
-    return pickup_ray.is_colliding()
+	return pickup_ray.is_colliding()
 
 func try_pickup_revolver() -> bool:
-    if !has_revolver:
-        pickup_ray.force_raycast_update()
+	if !has_revolver:
+		pickup_ray.force_raycast_update()
 
-        if pickup_ray.is_colliding():
-            var revolver: Revolver = pickup_ray.get_collider()
+		if pickup_ray.is_colliding():
+			var revolver: Revolver = pickup_ray.get_collider()
 
-            revolver.queue_free()
+			revolver.queue_free()
 
-            has_revolver = true
+			has_revolver = true
 
-            if revolver_status_animation_player.current_animation == "idle":
-                revolver_status_animation_player.play("fade_out")
-            
-            revolver_picked_up.emit()
+			if revolver_status_animation_player.current_animation == "idle":
+				revolver_status_animation_player.play("fade_out")
+			
+			revolver_picked_up.emit()
 
-            return true
+			return true
 
-    return false
+	return false
 
 func drop():
-    has_revolver = false
-    if !WaveSystem.instance.is_wave_active():
-        revolver_status_animation_player.play("fade_in")
+	has_revolver = false
+	if !WaveSystem.instance.is_wave_active():
+		revolver_status_animation_player.play("fade_in")
--- World/wave_system.gd
+++ World/wave_system.gd
@@ -8,6 +8,7 @@ signal wave_ended
 
 @export var enemy_spawn_locations: Array[Marker3D]
 @export var enemy_container: Node3D
+@export var wave_time_label: Label
 
 var enemy_scene: PackedScene = preload("uid://dqdin3vaff0vi")
 
@@ -21,50 +22,56 @@ var spawn_rate: float = 5.0
 var started: bool
 
 func _ready() -> void:
-    instance = self
+	instance = self
 
-    start_next_wave()
+	start_next_wave()
 
 func _process(delta: float) -> void:
-    if !started:
-        return
+	if !started:
+		return
 
-    wave_time -= delta
+	wave_time -= delta
+	wave_time = max(wave_time, 0.0)
 
-    if enemies_spawned < total_enemies:
-        spawn_time += delta
+	var minutes = int(wave_time) / 60.0
+	var seconds = int(wave_time) % 60
 
-        if spawn_time >= spawn_rate:
-            spawn_time = 0.0
-            _spawn_enemy()
-    else:
-        if get_enemy_count() == 0 and wave_time <= 0.0:
-           started = false
-           wave_ended.emit()
+	wave_time_label.text = "%02d:%02d" % [minutes, seconds]
+
+	if enemies_spawned < total_enemies:
+		spawn_time += delta
+
+	if spawn_time >= spawn_rate:
+		spawn_time = 0.0
+		_spawn_enemy()
+	else:
+		if get_enemy_count() == 0 and wave_time <= 0.0:
+			started = false
+			wave_ended.emit()
 
 func _spawn_enemy():
-    var spawn_location: Vector3 = enemy_spawn_locations.pick_random().global_position
+	var spawn_location: Vector3 = enemy_spawn_locations.pick_random().global_position
 
-    var enemy: NPC = enemy_scene.instantiate()
+	var enemy: NPC = enemy_scene.instantiate()
 
-    enemy_container.add_child(enemy)
+	enemy_container.add_child(enemy)
 
-    enemy.global_position = spawn_location
+	enemy.global_position = spawn_location
 
-    enemies_spawned += 1
+	enemies_spawned += 1
 
 func get_enemy_count() -> int:
-    return enemy_container.get_child_count()
+	return enemy_container.get_child_count()
 
 func start_next_wave():
-    wave_time = wave_duration
-    enemies_spawned = 0
-    spawn_time = spawn_rate
+	wave_time = wave_duration
+	enemies_spawned = 0
+	spawn_time = spawn_rate
 
-    started = true
-    current_wave += 1
+	started = true
+	current_wave += 1
 
-    wave_started.emit()
+	wave_started.emit()
 
 func is_wave_active() -> bool:
-    return started
+	return started
