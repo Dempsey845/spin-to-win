6bc85863e19461a20ced3cc414237c264ae50281
Dempsey845 <dempseynewton1@gmail.com>
2026-06-24 17:50:38 +0100

    Gave the NPC a punch sound effect.

--- NPC/Humanoid/npc_punch_manager.gd
+++ NPC/Humanoid/npc_punch_manager.gd
@@ -3,8 +3,11 @@ extends Node
 
 @export var punch_hitbox: Hitbox
 
+@onready var punch_stream_player: AudioStreamPlayer3D = $PunchStreamPlayer
+
 func enable_hitbox():
 	punch_hitbox.active = true
+	punch_stream_player.play()
 
 func disable_hitbox():
 	punch_hitbox.active = false
